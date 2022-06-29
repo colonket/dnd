69721b33706de8ccded3108c09370d09d6fec82d
Eric Iniguez <colonket@iniguez.dev>
2022-06-29 17:27:39 -0500

    roll dice, gen ability scores, get fantasy name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import random
+import fantasynames as names
 
 def listAttr():
     f = open("attributes.json","r")
@@ -21,21 +22,45 @@ def listAttr():
 
 class Character:
     def __init__(self, data):
-        self.level = 0
+        self.level = 1
         self.race = random.choice(data['race'])['name']
         self.charClass = random.choice(data['class'])['name']
-        self.name = random.choice(data['ability'])['short']
-        self.title = f"Level {self.level} {self.race} {self.charClass}"
+        self.name = names.human('any')
 
-        self.equipment = []
+        # Generate Ability Scores
+        self.AS = {}
+        for a in data['ability']:
+            self.AS[ a["short"] ] = rollAS()
+
+        self.hp = rollD(10)
+        self.speed = 30
+        self.init = 2
+        self.ac = 12
+        self.prof = 6
+            
+        self.backpack = []
         self.attacks = []
+
+        self.title = f"{self.name}, Lvl. {self.level} {self.race} {self.charClass}"
+        self.title += f"\n{self.AS}"
+        self.title += f"\nBackpack: {self.backpack}"
+        self.title += f"\nAttacks:  {self.attacks}"
 
     def __repr__(self):
         return self.title
 
     def __str__(self):
         return self.title
+    
 
+def rollD(value):
+    return random.randint(1,value) 
+
+def rollAS(r=4,k=3):
+    # Roll 'r' d6's, and keep the 'k' highest dice
+    rolls = [rollD(6) for i in range(r)]
+    return sum(sorted(rolls,reverse=True)[:3])
+        
 
 if __name__ == '__main__':
     with open("attributes.json","r",encoding="utf-8") as f:
