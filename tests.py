3736c1a93214db114cc039e09edb54cc6c4e3065
Eric Iniguez <colonket@iniguez.dev>
2022-07-01 10:46:35 -0500

    add test for Sard Roh

--- tests.py
+++ tests.py
@@ -31,10 +31,39 @@ def test_chars(ttrpg_json):
     print()
 
     print("Generating Random Character...")
-    npc1.generateRandom()
+    npc1.gen_random_char()
     print(npc1)
+
+
+
 
 if __name__ == '__main__':
     with open("dnd.json","r",encoding="utf-8") as f:
         dnd = json.loads(f.read())
-    test_chars(dnd)
+    #test_chars(dnd)
+
+    sard_roh = ttrpg_char.Character(
+        ttrpg_json=dnd,
+        level=2,
+        race="Tiefling",
+        char_class="Cleric 1 / Rogue 1",
+        name="Sard Roh",
+        abil_scores={
+            "STR":13,
+            "DEX":15,
+            "CON":13,
+            "INT":10,
+            "WIS":10,
+            "CHA":15
+        },
+        stats={
+            "HP":15,
+            "SPEED":30,
+            "INIT":2,
+            "AC":12,
+            "PROF":2
+        },
+        inventory=["Potion of Healing (Greater)","Dagger","Padded","Quarterstaff"],
+        actions=["Unarmed Strike"]
+    )
+    print(sard_roh)
